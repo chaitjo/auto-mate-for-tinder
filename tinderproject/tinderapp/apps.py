20a974702ec96257382e6898e0896799b91895b9
Chaitanya Joshi <ckjoshi9@gmail.com>
2016-01-27 19:39:22 +0800

    Removed duplicate directory

--- tinderproject/tinderapp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TinderappConfig(AppConfig):
-    name = 'tinderapp'
--- tinderproject/tinderapp/pynder/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .me import Profile
-from .message import Message
-from .user import Hopeful, Match
